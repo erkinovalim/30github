ab6ba4351e4b34922ba21455c2cd8084ea652507
Alim <alimerkinov43@gmail.com>
2025-02-08 22:57:50 +0600

    bfuy

--- main.cpp
+++ main.cpp
@@ -4,26 +4,29 @@
 
 using namespace std;
 
-int main(){
-    ll n; cin>>n;
-    ll mx = 1;
-    if(n % 2 == 1)
-        mx = n;
-    else{
-    for (ll i = 1; i <= sqrt(n); i++)
+ll twice(ll x)
+{
+    ll nx = x;
+    ll cp = x;
+
+    while(x > 0)
     {
-        if((n / i) % 2 == 1)
-        {
-            mx = n / i;
-            break;
-        }s
-    }}
+        x /= 10;
+        nx *= 10;
+    }
+
+    return nx + cp;
+}
+
+int main(){
+    ll a, b; cin>>a>>b;
+    int bruh = 0;
+    for (ll i = 1; i <= 10000000; i++)
+    {
+        ll ni = twice(i);
+        if(ni >= a && ni <= b)
+            bruh++;
+    }
     
-    cout<< n * n + mx;
-
-    // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-    // 1 1 3 1 5 3 7 1 9  5 11  3 13  7 15
-
-    // this works...
-    // now i need to find the fastest way to find the largest odd divisor. 
+    cout<<bruh;
 }
