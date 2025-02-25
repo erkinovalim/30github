f3689105c2138b42dd4a0a45410ac9300379f594
Alim <alimerkinov43@gmail.com>
2025-02-25 22:36:36 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -80,7 +80,7 @@ int main() {
         if(rects.size() & 1)
         {
             total += intersection(rects);
-      asdasd  }aasdaasdssdasdssdasdfasdfsdfsdasd
+      asdasd  }aasdaasdsm mn nk sdasdssdasdfasdfsdfsdasd
         yo in two days rayonniy else{
             total -= intersection(rects) * rects.size();
         }
